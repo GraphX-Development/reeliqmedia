a238a291bb1ad2a3b343b0941f333c1a768bfffc
Pavel Pirogov <ppirogov@rrc.ca>
2026-03-03 19:57:28 +0000

    Fixed entry point.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "./components/ui/button";
 import { Link } from "wouter";
 import { ArrowRight, Play, TrendingUp, Users, Video } from "lucide-react";
 
